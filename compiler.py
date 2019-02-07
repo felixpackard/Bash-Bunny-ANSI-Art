1944ae19bea03f1f0c03f8850548258fe4ba5cf3
Felix Packard <fjpackard@gmail.com>
2019-02-07 23:10:04 +0000

    Added compatability with Python 2.7

--- compiler.py
+++ compiler.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python
 
+from __future__ import with_statement
+from __future__ import absolute_import
 import sys
+from io import open
 
 if (len(sys.argv) != 3):
-	sys.exit("Usage: " + sys.argv[0] + " [ascii-art-in] [payload-out]")
+	sys.exit(u"Usage: " + sys.argv[0] + u" [ascii-art-in] [payload-out]")
 
 inname = sys.argv[1]
 outname = sys.argv[2]
 
-infile = open(inname, "r")
-outfile = open(outname, "w")
-with open("altcodes.txt", "r") as f:
+infile = open(inname, u"r")
+outfile = open(outname, u"w")
+with open(u"altcodes.txt", u"r") as f:
 	altcodes = f.readlines()
 
 # Write start of file
-outfile.write("LED SETUP\nATTACKMODE HID\nLED ATTACK\nQUACK GUI r\nQUACK DELAY 500\nQUACK STRING \"cmd /K copy con ansi.txt&exit\"\nQUACK ENTER\nQUACK DELAY 1000\n\n")
+outfile.write(u"LED SETUP\nATTACKMODE HID\nLED ATTACK\nQUACK GUI r\nQUACK DELAY 500\nQUACK STRING \"cmd /K copy con ansi.txt&exit\"\nQUACK ENTER\nQUACK DELAY 1000\n\n")
 
 # 'Compile' to ALT-Codes and append to file
 for line in infile:
-	newline = ""
+	newline = u""
 	for char in line:
 		for altline in altcodes:
-			if char in altline.strip().split(" ")[-1]:
-				newline += "QUACK ALTCODE " + str(altline.strip().split(" ")[0]) + "\n"
-	outfile.write(newline + "QUACK enter\n\n")
+			if char in altline.strip().split(u" ")[-1]:
+				newline += u"QUACK ALTCODE " + unicode(altline.strip().split(u" ")[0]) + u"\n"
+	outfile.write(newline + u"QUACK enter\n\n")
 
-print("Payload written to " + outname)
+print u"Payload written to " + outname
